80fa102154c7cad4e1eca75762958394f66927f0
jicahoo <jicahoo@163.com>
2017-12-09 12:12:40 +0800

    A version with bug: loop for ever

---
+++ src/main/java/com/hello/echo/server/NioEchoServer.java
@@ -0,0 +1,73 @@
+package com.hello.echo.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NioEchoServer {
+    public void server(int port) throws IOException {
+        System.out.println("Listening for connections on port " + port);
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        ServerSocket ss = serverChannel.socket();
+        InetSocketAddress addr = new InetSocketAddress(port);
+        ss.bind(addr);
+        serverChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // A version with bug loop forever.
+        while (true) {
+            System.out.println("Before select");
+            int evtNumber = selector.select();
+            System.out.println("After select");
+            System.out.println("Event number: " + evtNumber);
+
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+
+            while (iterator.hasNext()) {
+                System.out.println("Has more event.");
+                SelectionKey key = iterator.next();
+                iterator.remove();
+
+                if (key.isAcceptable()) {
+                    System.out.println("isAcceptable.");
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel client = server.accept();
+                    System.out.println("Accepted connection from " + client);
+                    client.configureBlocking(false);
+                    client.register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE,
+                            ByteBuffer.allocate(100));
+                }
+
+                if (key.isReadable()) {
+                    System.out.println("isReadable");
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    ByteBuffer output = (ByteBuffer) key.attachment();
+                    channel.read(output);
+                    System.out.println(output.toString());
+                }
+
+                if (key.isWritable()) {
+                    System.out.println("isWritable");
+                    SocketChannel client = (SocketChannel) key.channel();
+                    ByteBuffer output = (ByteBuffer) key.attachment();
+                    output.flip();
+                    client.write(output);
+                    output.compact();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioEchoServer().server(9999);
+    }
+}
