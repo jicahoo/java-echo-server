95033159ac658a351d796c0c0ff1ebfebdb745d2
jicahoo <jicahoo@163.com>
2017-12-10 15:57:40 +0800

    Add test

--- tests/python/test-echo-server.py
+++ tests/python/test-echo-server.py
@@ -1,2 +1,14 @@
+#py27
+import socket
+
 if __name__ == '__main__':
-    print "Hello Python";
+    for i in range(10):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('127.0.0.1',9999))
+        msg = "Hello: %s" % i
+        s.send(msg)
+        r = s.recv(1024)
+        print r
+        print msg == r
+        assert msg == r
+        s.close()
