a229ca44c637d624c1f17ba927cbb3a8d05e88ee
jicahoo <jicahoo@163.com>
2017-12-11 20:13:43 +0800

    fix test script bug #1

--- tests/python/test-echo-server.py
+++ tests/python/test-echo-server.py
@@ -5,15 +5,20 @@ import time
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('127.0.0.1', 9999))
+    r = ""
+    sr = ""
     for i in range(10):
-        msg = "012-%s" % i
+        msg = "0123456789-%s" % i
+        sr += msg
         print "send %s" % msg
         s.sendall(msg)
-        r = s.recv(1024)
-        print r
         #print msg == r
-        assert msg == r
+        #assert msg == r
     # shutdown gracefully, or server won't recieive EOF which will lead to 
     # exception.
+    r = s.recv(1024)
+    while r[-2:] != "-9":
+        r += s.recv(1024)
+    assert r == sr
     s.shutdown(socket.SHUT_RDWR)
     s.close()
